7cc9e1d8fb543966c7ebac1c37bfd1d64a36aa70
Alexander Teinum <ateinum@gmail.com>
2013-01-22 14:25:00 +0100

    Remove unused code

--- js/models/test-run.coffee
+++ js/models/test-run.coffee
@@ -8,18 +8,6 @@ define ['backbone'], (Backbone) ->
       this.set 'tests', tests
     progress: () ->
       parseInt((this.get('currentTest') / (this.get('tests').length - 1)) * 100)
-    previousTest: () ->
-      if this.get('currentTest') == 0
-        return false
-
-      this.set('currentTest', this.get('currentTest') - 1)
-      return true
-    nextTest: () ->
-      if (this.get('currentTest') == (this.get('tests').length - 1))
-        return false
-
-      this.set('currentTest', this.get('currentTest') + 1);
-      return true
     isAtLast: () ->
       this.get('currentTest') == (this.get('tests').length - 1)
   }
--- js/models/test-run.js
+++ js/models/test-run.js
@@ -13,20 +13,6 @@
       progress: function() {
         return parseInt((this.get('currentTest') / (this.get('tests').length - 1)) * 100);
       },
-      previousTest: function() {
-        if (this.get('currentTest') === 0) {
-          return false;
-        }
-        this.set('currentTest', this.get('currentTest') - 1);
-        return true;
-      },
-      nextTest: function() {
-        if (this.get('currentTest') === (this.get('tests').length - 1)) {
-          return false;
-        }
-        this.set('currentTest', this.get('currentTest') + 1);
-        return true;
-      },
       isAtLast: function() {
         return this.get('currentTest') === (this.get('tests').length - 1);
       }
