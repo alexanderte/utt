f302ad99524a880380426ca0ec0d228b5d67992c
Lynn Holdsworth <lynn.holdsworth@tingtun.no>
2014-03-05 13:42:44 +0000

    Hopefully all pesky BOM marks gone now

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ fi
 git reset --hard
 git pull
 
-sed -i "s|4563|$2|" backend/main.js
+sed -i "s|4563|$2|" backend/main.coffee
 sed -i "s|//localhost:4563|$1:$2|" frontend/main.js
 
 nohup coffee backend/main.coffee&
